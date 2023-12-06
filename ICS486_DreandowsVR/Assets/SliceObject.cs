59d2b8a73b1d825cd9f0a978b28e26e95e60721a
kachamaru <aloha.reirei1@gmail.com>
2023-12-06 12:23:32 -1000

    SwordsandStuf

---
+++ ICS486_DreandowsVR/Assets/SliceObject.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EzySlice;
+using UnityEngine.InputSystem;
+
+
+public class SliceObject : MonoBehaviour
+{
+    public Transform startSlicePoint;
+    public Transform endSlicePoint;
+    public VelocityEstimator velocityEstimator;
+    public LayerMask sliceableLayer;
+
+    public Material crossSectionMaterial;
+    public float cutForce = 2000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        bool hasHit = Physics.Linecast(startSlicePoint.position, endSlicePoint.position, out RaycastHit hit, sliceableLayer);
+        if (hasHit)
+        {
+            GameObject target = hit.transform.gameObject;
+            Slice(target);
+        }
+    }
+
+    public void Slice(GameObject target)
+    {
+        Vector3 velocity = velocityEstimator.GetVelocityEstimate();
+        Vector3 planeNormal = Vector3.Cross(endSlicePoint.position - startSlicePoint.position, velocity);
+        planeNormal.Normalize();
+
+        SlicedHull hull = target.Slice(endSlicePoint.position, planeNormal);
+
+        if (hull != null)
+        {
+            GameObject upperHull = hull.CreateUpperHull(target, crossSectionMaterial);
+            SetupSlicedComponent(upperHull);
+
+            GameObject lowerHull = hull.CreateLowerHull(target, crossSectionMaterial);
+            SetupSlicedComponent(lowerHull);
+
+            Destroy(target);
+        }
+    }
+
+    public void SetupSlicedComponent(GameObject slicedObject)
+    {
+        Rigidbody rb = slicedObject.AddComponent<Rigidbody>();
+        MeshCollider collider = slicedObject.AddComponent<MeshCollider>();
+        collider.convex = true;
+        rb.AddExplosionForce(cutForce, slicedObject.transform.position, 1);
+    }
+
+    /* void Update()
+     {
+         if (Keyboard.current.spaceKey.wasPressedThisFrame)
+         {
+             Slice(target);
+         }
+
+     }
+
+     public void Slice(GameObject target)
+     {
+         SlicedHull hull = target.Slice(planeDebug.position, planeDebug.up);
+
+         if(hull != null)
+         {
+             GameObject upperHull = hull.CreateUpperHull(target, crossSectionMaterial);
+             GameObject lowerHull = hull.CreateLowerHull(target, crossSectionMaterial);
+         }
+
+     }
+ */
+
+
+
+
+}
+
